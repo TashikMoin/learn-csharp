fdbf99a729d848301f8c9910b2700160fb361b49
TashikMoin <tashikmoinsheikh@gmail.com>
2021-12-30 05:35:10 +0500

    added wcf

---
+++ WCF/ConsoleWCFClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleWCFClient.ProxyNamespace;
+using System;
+
+namespace ConsoleWCFClient
+{
+
+    // Run WCF service Host Project as an administrator.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WCFServiceClient Obj = new WCFServiceClient("BasicHttpBinding_IWCFService");
+            Console.WriteLine(Obj.Greet("Tashik"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WCF/WCFService/WCFService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WCFService" in both code and config file together.
+    public class WCFService : IWCFService
+    {
+        public string Greet(string Name)
+        {
+            return "Hello, " + Name;
+        }
+    }
+}
